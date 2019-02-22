b93a92e77f8c3f9d89a96363298d026b7d3108a8
Long Bui <longbui68@yahoo.de>
2019-02-22 19:14:00 +0100

    Update namespaces and add TCP service support

--- kubernetes/cluster-setup.sh
+++ kubernetes/cluster-setup.sh
@@ -6,19 +6,19 @@ kubectl apply -f ./helm
 helm init --service-account tiller --upgrade
 
 # Metrics server
-helm install --name metrics-server --namespace utilities --values ./metrics-server/values.yaml stable/metrics-server
+helm install --name metrics-server --namespace kube-system --values ./metrics-server/values.yaml stable/metrics-server
 
 # Dashboard
 helm install --name dashboard --namespace kube-system --values ./dashboard/values.yaml stable/kubernetes-dashboard
 kubectl apply -f ./dashboard/user.yaml
 
 # Ingress
-helm install --name ingress --values ./ingress-controller/values.yaml --namespace utilities stable/nginx-ingress
+helm install --name ingress --values ./ingress-controller/values.yaml --namespace ingress-controller stable/nginx-ingress
 
 # Let's Encrypt for Ingresses
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
 sleep 5
-kubectl label namespace utilities certmanager.k8s.io/disable-validation=true
-helm install --name cert-manager --namespace utilities stable/cert-manager
+kubectl label namespace ingress-controller certmanager.k8s.io/disable-validation=true
+helm install --name cert-manager --namespace ingress-controller stable/cert-manager
 sleep 5
 kubectl apply -f ./ingress-controller/cert-manager
