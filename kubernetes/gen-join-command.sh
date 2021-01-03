91f2f18e0a452198abb543e401674add5929326f
Long Bui <hello@long-bui.de>
2021-01-03 16:04:33 +0100

    Change container runtime to containerd

--- kubernetes/gen-join-command.sh
+++ kubernetes/gen-join-command.sh
@@ -1,3 +1,2 @@
 # Use on Master Node to generate join command to be used on new nodes
-echo "kubeadm join --token $(kubeadm token create) $(hostname -I | awk '{print $1;}'):6443 --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
-   openssl dgst -sha256 -hex | sed 's/^.* //')"
+echo "kubeadm join --token $(kubeadm token create) $(hostname -I | awk '{print $1;}'):6443 --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')"
--- kubernetes/node-setup.sh
+++ kubernetes/node-setup.sh
@@ -3,40 +3,44 @@ swapoff -a
 apt-get update
 
 mkdir /etc/systemd/system/kubelet.service.d
+
+# Default kubelet config
 cat <<EOF >/etc/systemd/system/kubelet.service.d/20-hetzner-cloud.conf
 [Service]
 Environment="KUBELET_EXTRA_ARGS=--cloud-provider=external"
 EOF
 
 # For static CPU Manager Policy
-# mkdir /etc/systemd/system/kubelet.service.d
 # cat <<EOF >/etc/systemd/system/kubelet.service.d/20-hetzner-cloud.conf
 # [Service]
 # Environment="KUBELET_EXTRA_ARGS=--cloud-provider=external --cpu-manager-policy=static --kube-reserved=cpu=100m,memory=1Gi,ephemeral-storage=1Gi --system-reserved=cpu=100m,memory=1Gi,ephemeral-storage=1Gi"
 # EOF
 
-mkdir /etc/systemd/system/docker.service.d
-cat <<EOF >/etc/systemd/system/docker.service.d/00-cgroup-systemd.conf
-[Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --exec-opt native.cgroupdriver=systemd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
 EOF
-systemctl daemon-reload
+
+modprobe overlay
+modprobe br_netfilter
 
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/docker-and-kubernetes.list
-        deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
         deb http://packages.cloud.google.com/apt/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y docker-ce kubelet=1.17.3-00 kubeadm=1.17.3-00 kubectl=1.17.3-00
-apt-mark hold docker-ce kubelet kubeadm kubectl
+apt-get install -y containerd kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
+apt-mark hold containerd kubelet kubeadm kubectl
+
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+sed -i '/\[plugins."io.containerd.grpc.v1.cri".cni\]/i \\t  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]\n\t    SystemdCgroup = true' /etc/containerd/config.toml
 
 cat <<EOF >>/etc/sysctl.conf
 # Allow IP forwarding for kubernetes
 net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 net.ipv6.conf.default.forwarding = 1
 EOF
